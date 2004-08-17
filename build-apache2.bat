b36378e545e031eeeae795025999798c2db04a4c
Edward Rudd <urkle@outoforder.cc>
2004-08-17 15:13:13 +0000

    Added SSL building for win32

--- build-apache2.bat
+++ build-apache2.bat
@@ -7,6 +7,10 @@ rem path to mysql 4.0 installation
 SET DIR_MYSQL=C:\MySQL
 rem Can be set to opt or debug
 SET LIB_MYSQL=opt
+rem path to OpenSSL installation
+SET DIR_OPENSSL=C:\OpenSSL
+rem Should be set to VC
+SET LIB_OPENSSL=VC
 
 copy /Y winconfig.h config.h
 mkdir Release
@@ -19,7 +23,13 @@ echo /I.. >> RESP_c.txt
 echo /I"%DIR_MSSDK%\Include" >> RESP_c.txt
 echo /I"%DIR_APACHE%\Include" >> RESP_c.txt
 echo /I"%DIR_MYSQL%\Include" >> RESP_c.txt
+echo /I"%DIR_OPENSSL%\Include" >> RESP_c.txt
+echo /I"%DIR_OPENSSL%\Include\openssl" >> RESP_c.txt
 cl @RESP_c.txt /c ..\mod_log_sql.c ..\mod_log_sql_mysql.c
+
+if not exist "%DIR_APACHE%\Include\mod_ssl.h" goto nossl
+cl @RESP_C.txt /c ..\mod_log_sql_ssl.c
+:nossl
 
 rem link main module
 echo /MACHINE:I386 /SUBSYSTEM:windows > RESP_l.txt
@@ -37,4 +47,16 @@ echo /NODEFAULTLIB:LIBCMT.lib >> RESP_l.txt
 echo libapr.lib libaprutil.lib libhttpd.lib >> RESP_l.txt
 echo libmysql.lib mod_log_sql.lib >> RESP_l.txt
 link @RESP_l.txt mod_log_sql_mysql.obj
+
+if not exist "%DIR_APACHE%\Include\mod_ssl.h" goto done
+rem link ssl module
+echo /MACHINE:I386 /SUBSYSTEM:windows > RESP_l.txt
+echo /OUT:mod_log_sql_ssl.so /DLL /OPT:REF /DEBUG >> RESP_l.txt
+echo /LIBPATH:"%DIR_APACHE%\lib" >> RESP_l.txt
+echo /LIBPATH:"%DIR_OPENSSL%\lib\%LIB_OPENSSL%" >> RESP_l.txt
+echo libapr.lib libaprutil.lib libhttpd.lib >> RESP_l.txt
+echo mod_log_sql.lib >> RESP_l.txt
+link @RESP_l.txt mod_log_sql_ssl.obj
+
+:done
 cd ..
