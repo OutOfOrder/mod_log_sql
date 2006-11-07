ee22f4a4df3632c5d6def4ef108cbdbda0ce4e47
Edward Rudd <urkle@outoforder.cc>
2006-11-07 02:44:33 +0000

    updated license file updated documentation

--- functions20.h
+++ functions20.h
@@ -107,7 +107,7 @@ static const char *extract_connection_status(request_rec *r, char *a)
     if (r->connection->aborted)
         return "X";
 
-    if (r->connection->keepalive == AP_CONN_KEEPALIVE && 
+    if (r->connection->keepalive == AP_CONN_KEEPALIVE &&
         (!r->server->keep_alive_max ||
          (r->server->keep_alive_max - r->connection->keepalives) > 0)) {
         return "+";
