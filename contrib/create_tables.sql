c041901c47a50a313cb603c42ccba33b3560db98
John Hansen <john@mrhansen.id.au>
2020-06-20 23:43:46 +1000

    update database schema in contrib/create_tables.sql, now works with both MySQL/MariaDB and PostgreSQL

--- contrib/create_tables.sql
+++ contrib/create_tables.sql
@@ -1,51 +1,90 @@
-create table access_log (
-	id char(19) ,
-	agent varchar(255) ,
-	bytes_sent int unsigned ,
-	child_pid smallint unsigned,
-	cookie varchar(255),
-	machine_id varchar(25),
-	request_file varchar(255),
-	referer varchar(255) ,
-	remote_host varchar(50) ,
-	remote_logname varchar(50) ,
-	remote_user varchar(50) ,
-	request_duration smallint unsigned ,
-	request_line varchar(255),
-	request_method varchar(10) ,
-	request_protocol varchar(10) ,
-	request_time char(28),
-	request_uri varchar(255),
-	request_args varchar(255),
-	server_port smallint unsigned,
-	ssl_cipher varchar(25),
-	ssl_keysize smallint unsigned,
-	ssl_maxkeysize smallint unsigned,
-	status smallint unsigned ,
-	time_stamp int unsigned ,
-	virtual_host varchar(255)
+CREATE TABLE apachelog (
+    id character(28) NULL UNIQUE,
+    service character varying(12) DEFAULT 'APACHE' NOT NULL,
+    agent character varying(255),
+    bytes_sent integer,
+    bytes_recvd integer,
+    child_pid integer,
+    child_tid bigint,
+    cookie character varying(255),
+    machine_id character varying(25),
+    request_file character varying(255),
+    referer character varying(255),
+    local_address character(15),
+    server_name character varying(255),
+    remote_address character(15),
+    remote_host character varying(50),
+    remote_logname character varying(50),
+    remote_user character varying(50),
+    request_duration integer,
+    request_line character varying(255),
+    request_method character varying(16),
+    request_protocol character varying(10),
+    request_time timestamp,
+    request_uri character varying(255),
+    request_args character varying(255),
+    server_port integer,
+    status integer,
+    request_timestamp integer,
+    virtual_host character varying(255),
+    connection_status character(1),
+    win32status integer,
+    ssl_cipher character varying(25),
+    ssl_keysize integer,
+    ssl_maxkeysize integer
 );
 
-create table notes (
-    id char(19),
-    item varchar(80),
-    val varchar(80)
+CREATE TABLE notes (
+    id character(28) NOT NULL,
+    item character varying(80),
+    val character varying(255),
+    FOREIGN KEY(id) REFERENCES apachelog(id)
+      ON DELETE CASCADE
+      ON UPDATE CASCADE
 );
 
-create table headers_in (
-    id char(19),
-    item varchar(80),
-    val varchar(80)
+CREATE TABLE headers_in (
+    id character(28) NOT NULL,
+    item character varying(80),
+    val character varying(255),
+    FOREIGN KEY(id) REFERENCES apachelog(id)
+      ON DELETE CASCADE
+      ON UPDATE CASCADE
 );
 
-create table headers_out (
-    id char(19),
-    item varchar(80),
-    val varchar(80)
+CREATE TABLE headers_out (
+    id character(28) NOT NULL,
+    item character varying(80),
+    val character varying(255),
+    FOREIGN KEY(id) REFERENCES apachelog(id)
+      ON DELETE CASCADE
+      ON UPDATE CASCADE
 );
 
-create table cookies (
-    id char(19),
-    item varchar(80),
-    val varchar(80)
+CREATE TABLE cookies (
+    id character(28) NOT NULL,
+    item character varying(80),
+    val character varying(255),
+    FOREIGN KEY(id) REFERENCES apachelog(id)
+      ON DELETE CASCADE
+      ON UPDATE CASCADE
 );
+
+CREATE VIEW inetlog AS
+ SELECT
+    remote_host AS "ClientHost",
+    remote_user AS username,
+    request_time AS "LogTime",
+    service,
+    machine_id AS machine,
+    local_address AS serverip,
+    remote_address AS clientip,
+    request_duration AS processingtime,
+    bytes_recvd AS bytesrecvd,
+    bytes_sent AS bytessent,
+    status AS servicestatus,
+    win32status,
+    request_method AS operation,
+    request_uri AS target,
+    request_args AS parameters
+ FROM apachelog;
